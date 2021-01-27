fd8dc85f9c26e8a1ae362ff62957038aec9443e6
gundu pooja <S538295@nwmissouri.edu>
2021-01-27 09:55:42 -0600

    added event handlers and main method in main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,75 +1,43 @@
-  
-// import default object with a local camelCase name
-import locationsArray from '../init-locations.js';
+/**
+ * Main.js - This is where the logic for our app resides
+ */
 
-const inside = (device, bounds) => {
-  console.log(`CHECKING inside ${bounds.Name}`);
-  console.log(device);
-  console.log(bounds);
-  console.log(device.latitude > bounds.South);
-  console.log(device.latitude < bounds.North);
-  console.log(device.longitude > bounds.West);
-  console.log(device.longitude < bounds.East);
-  const ans =
-    device.latitude > bounds.South &&
-    device.latitude < bounds.North &&
-    device.longitude > bounds.West &&
-    device.longitude < bounds.East;
-   console.log(`CHECKING ${bounds.Name} ANS: ${ans}`);
-  return ans;
-};
+// first import
+import getLocation from './location.js';
+
+// helper functions
+
+
+// event handlers
 
 /**
- * Get the location
- * Uses new import / export - be sure to set type="module" in HTML
- * Can be easily added to any web page.
- * Includes GeoLocation API example.
- * @module location/getLocation
- * @author pooja gundu
+ * Wait to get location and then display it.
+ * Location should only be updated in response to a USER GESTURE
  */
-export default function getLocation() {
-  if (!navigator.geolocation) {
-    document.querySelector('#error-message').innerHTML =
-      'Browser does not support geolocation.';
-  } else {
-    const options = {
-      enableHighAccuracy: true,
-      timeout: 5000,
-      maximumAge: 0,
-    };
-
-    navigator.geolocation.getCurrentPosition(
-      (position) => {
-        document.querySelector('#device-lat').innerHTML = '';
-        document.querySelector('#device-long').innerHTML = '';
-        document.querySelector('#locationAnswer').innerHTML = 'The Hunt Game';
-
-        if (position === undefined) {
-          document.querySelector('#error-message').innerHTML =
-            'Browser cannot determine device position (position is undefined).';
-        }
-        const device = position.coords;
-        document.querySelector('#device-lat').innerHTML = device.latitude;
-        document.querySelector('#device-long').innerHTML = device.longitude;
-        const arrayLength = locationsArray.length;
-        for (let i = 0; i < arrayLength; i += 1) {
-          const thisLoc = locationsArray[i];
-          if (inside(device, thisLoc)) {
-            const name = thisLoc.Name;
-            document.querySelector('#locationAnswer').innerHTML = name;
-            const utterance = new SpeechSynthesisUtterance();
-            utterance.text = `Congratulations! You found location ${name}`;
-            window.speechSynthesis.speak(utterance);
-            break;
-          }
-        }
-      },
-      (err) => {
-        const s = `ERROR(${err.code}): ${err.message}`;
-        console.warn(s);
-        document.querySelector('#error-message').innerHTML = err.message;
-      },
-      options,
-    );
-  }
+async function locationHandler() {
+    const locText = await getLocation();
+    document.getElementById('locationAnswer').innerHTML = locText;
 }
+
+function clearErrorText() {
+    document.getElementById('error-message').innerHTML = '';
+}
+
+// declaration of the main method
+function main() {
+    console.log('Starting main method..');
+
+    // get references to html elements
+    const locationElement = document.getElementById('location');
+    const errorElement = document.getElementById('error-message');
+
+    // initialize the error to empty string
+    errorElement.innerHTML = '';
+
+    locationElement.addEventListener('click', locationHandler);
+    locationElement.addEventListener('touch', locationHandler);
+}
+// this is where it begins
+window.addEventListener('load', main);
+
+
